dba3d49b69645a4fe4964883bbd43dc344871066
Xiaoxuan Wang <xiaoxuanwang@Xiaoxuans-MacBook-Pro.local>
2019-09-10 21:40:17 -0700

    Fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,13 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 //const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 //const devMode = process.env.NODE_ENV !== 'production';
 
-
 module.exports = {
   entry: "./src/client/index.js",
   output: {
     path: path.join(__dirname, "./src/client/build"),
     filename: "bundle.js",
     //publicPath allows you to specify the base path for all the assets within your application
-    publicPath:'/'
+    publicPath: "/"
   },
   module: {
     rules: [
@@ -24,24 +23,21 @@ module.exports = {
         }
       },
       {
-      test: /\.(s*)css$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [
-            'css-loader',
-            'sass-loader'
-          ]
+        test: /\.(s*)css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "sass-loader"]
         })
       },
       {
-      test: /\.(jpg|png|gif|svg|woff|woff2|eot|ttf)$/,
-      use: {
-        loader: "url-loader",
-        options: {
-          limit: 25000,
-        },
-      },
-    }
+        test: /\.(jpg|png|gif|svg|woff|woff2|eot|ttf)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 25000
+          }
+        }
+      }
     ]
   },
   devServer: {
@@ -50,22 +46,23 @@ module.exports = {
     port: 3000,
     open: true,
     proxy: {
-      "/api":{
-        target:"http://localhost:8080",
-        secure:false,
-        changeOrigin:true,
-        pathRewrite:{
-          "^/api":"/api"
+      "/api": {
+        target: "http://localhost:8080",
+        secure: false,
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "/api"
         }
       }
     }
   },
   plugins: [
-    new ExtractTextPlugin('./src/client/styles/css/mystyles.css'),
+    new ExtractTextPlugin("./src/client/styles/css/mystyles.css"),
     //new CleanWebpackPlugin([outputDirectory]),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
       favicon: "./public/favicon.ico"
-    })
+    }),
+    new CleanWebpackPlugin()
   ]
 };
