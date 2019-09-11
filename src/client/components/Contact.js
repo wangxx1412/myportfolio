eda80e3198d89258bef3cd08773f4171994aaf7d
Xiaoxuan Wang <xiaoxuanwang@Xiaoxuans-MacBook-Pro.local>
2019-09-10 21:22:14 -0700

    Update

--- src/client/components/Contact.js
+++ src/client/components/Contact.js
@@ -1,19 +1,17 @@
-import React, {Component} from 'react';
-import { Link } from "react-router-dom";
-import axios from 'axios';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import React, { Component } from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
-class Contact extends Component{
+class Contact extends Component {
   constructor() {
     super();
     this.state = {
       fields: {},
       errors: {}
-    }
+    };
     this.handleChange = this.handleChange.bind(this);
-
-  };
+  }
 
   handleChange(e) {
     let fields = this.state.fields;
@@ -24,89 +22,90 @@ class Contact extends Component{
   }
 
   validateForm() {
-      let fields = this.state.fields;
-      let errors = {};
-      var formIsValid = true;
+    let fields = this.state.fields;
+    let errors = {};
+    var formIsValid = true;
 
-      if (!fields["name"]) {
+    if (!fields["name"]) {
+      formIsValid = false;
+      errors["name"] = "*Please enter your username.";
+    }
+
+    if (typeof fields["name"] !== "undefined") {
+      if (!fields["name"].match(/^[a-zA-Z ]*$/)) {
         formIsValid = false;
-        errors["name"] = "*Please enter your username.";
+        errors["name"] = "*Please enter alphabet characters only.";
       }
+    }
 
-      if (typeof fields["name"] !== "undefined") {
-        if (!fields["name"].match(/^[a-zA-Z ]*$/)) {
-          formIsValid = false;
-          errors["name"] = "*Please enter alphabet characters only.";
-        }
-      }
+    if (!fields["email"]) {
+      formIsValid = false;
+      errors["email"] = "*Please enter your email.";
+    }
 
-      if (!fields["email"]) {
+    if (typeof fields["email"] !== "undefined") {
+      //regular expression for email validation
+      var pattern = new RegExp(
+        /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+      );
+      if (!pattern.test(fields["email"])) {
         formIsValid = false;
-        errors["email"] = "*Please enter your email.";
+        errors["email"] = "*Please enter valid email.";
       }
+    }
 
-      if (typeof fields["email"] !== "undefined") {
-        //regular expression for email validation
-        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-        if (!pattern.test(fields["email"])) {
-          formIsValid = false;
-          errors["email"] = "*Please enter valid email.";
-        }
+    this.setState({
+      errors: errors
+    });
+    return formIsValid;
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.validateForm()) {
+      //reset the fields
+      let fields = {};
+      fields["name"] = "";
+      fields["email"] = "";
+      this.setState({ fields: fields });
+
+      //send the message through nodemailer
+      const FormData = {
+        Name: document.getElementById("name").value,
+        Email: document.getElementById("email").value,
+        Message: document.getElementById("message").value
       };
 
-      this.setState({
-        errors: errors
+      axios.post("/api/send", FormData).then(response => {
+        if (response.data.msg === "success") {
+          console.log(FormData);
+          alert("Message Sent.");
+          this.resetForm();
+        } else if (response.data.msg === "fail") {
+          alert("Message failed to send.");
+        }
       });
-      return formIsValid;
     }
+  }
 
-    handleSubmit(e){
-        e.preventDefault();
-        if (this.validateForm()) {
-        //reset the fields
-        let fields = {};
-        fields["name"] = "";
-        fields["email"] = "";
-        this.setState({fields:fields});
+  resetForm() {
+    document.getElementById("contact-form").reset();
+  }
 
-        //send the message through nodemailer
-        const FormData = {
-          Name: document.getElementById('name').value,
-          Email:document.getElementById('email').value,
-          Message:document.getElementById('message').value
-        };
-
-        axios.post(
-          '/api/send',
-          FormData
-        )
-          .then((response)=>{
-            if (response.data.msg === 'success'){
-              console.log(FormData);
-              alert("Message Sent.");
-              this.resetForm()
-            }else if(response.data.msg === 'fail'){
-              alert("Message failed to send.")
-            }
-        })
-      }
-    }
-
-    resetForm(){
-        document.getElementById('contact-form').reset();
-    }
-
-  render(){
-    return(
+  render() {
+    return (
       <div className="container contact">
         <header className="contact-header">
           <div className="columns is-mobile is-centered">
-            <a className="column is-narrow contact-header__text has-text-black"  href="/">
+            <a
+              className="column is-narrow contact-header__text has-text-black"
+              href="/"
+            >
               <p className="is-size-6 has-text-centered">HOME</p>
             </a>
           </div>
         </header>
-        <hr/>
+        <hr />
         <section className="thx-msg">
           <div className="container">
             <p className="title is-size-1-tablet is-size-3-mobile has-text-centered">
@@ -116,11 +115,24 @@ class Contact extends Component{
         </section>
         <div className="columns is-mobile">
           <div className="column is-10-mobile is-offset-1-mobile is-three-fifths-tablet is-offset-one-fifth-tablet">
-            <form className="contact-form" id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST" action="http://localhost:4000/api/send">
+            <form
+              className="contact-form"
+              id="contact-form"
+              onSubmit={this.handleSubmit.bind(this)}
+              method="POST"
+              action="http://localhost:4000/api/send"
+            >
               <div className="field">
                 <label className="label">Name</label>
                 <p className="control  has-icons-left">
-                  <input className="input is-medium is-radiusless" name="name" onChange={this.handleChange} id="name" type="text" placeholder="e.g. Alex Smith" />
+                  <input
+                    className="input is-medium is-radiusless"
+                    name="name"
+                    onChange={this.handleChange}
+                    id="name"
+                    type="text"
+                    placeholder="e.g. Alex Smith"
+                  />
                   <span className="icon is-small is-left">
                     <FontAwesomeIcon icon={faUser} />
                   </span>
@@ -130,7 +142,14 @@ class Contact extends Component{
               <div className="field">
                 <label className="label">Email</label>
                 <p className="control  has-icons-left">
-                  <input className="input is-medium is-radiusless" name="email"  onChange={this.handleChange}  id="email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+                  <input
+                    className="input is-medium is-radiusless"
+                    name="email"
+                    onChange={this.handleChange}
+                    id="email"
+                    type="email"
+                    placeholder="e.g. alexsmith@gmail.com"
+                  />
                   <span className="icon is-small is-left">
                     <FontAwesomeIcon icon={faEnvelope} />
                   </span>
@@ -139,12 +158,28 @@ class Contact extends Component{
               </div>
               <div className="field">
                 <label className="label">Message</label>
-                <div className="control" >
-                  <textarea className="textarea is-medium is-radiusless" id="message" placeholder="Textarea" rows="10"></textarea>
+                <div className="control">
+                  <textarea
+                    className="textarea is-medium is-radiusless"
+                    id="message"
+                    placeholder="Textarea"
+                    rows="10"
+                  ></textarea>
                 </div>
               </div>
-              <button className="button is-medium  is-info is-outlined" type="submit">Send</button>
-              <a className="button is-medium  is-danger is-outlined" type="reset" onClick={this.resetForm.bind(this)}>Reset</a>
+              <button
+                className="button is-medium  is-info is-outlined"
+                type="submit"
+              >
+                Send
+              </button>
+              <a
+                className="button is-medium  is-danger is-outlined"
+                type="reset"
+                onClick={this.resetForm.bind(this)}
+              >
+                Reset
+              </a>
             </form>
           </div>
         </div>
--- src/client/components/Resume.js
+++ src/client/components/Resume.js
@@ -1,50 +1,84 @@
 import React, { Component } from "react";
 
-class Resume extends Component{
-  render(){
-    return(
+class Resume extends Component {
+  render() {
+    return (
       <section className="resume">
         <div className="container is-fullhd">
           <div className="columns is-desktop is-vcentered">
             <div className="skill-title column  is-4-desktop is-offset-1-desktop is-11-touch is-offset-1-touch">
-              <p className="titleIntro is-size-7 has-text-warning">MY SKILLS ARE</p>
-              <p className="title is-1 is-size-1-tablet is-size-3-mobile">FULL STACK DEVELOPING & MULTI-PHYSICS SIMULATION</p>
-              <p className="subtitle is-3 is-size-6 is-weight-normal has-text-grey-dark">If you are interested, please check my Resume through the button below.</p>
+              <p className="titleIntro is-size-7 has-text-warning">
+                MY SKILLS ARE
+              </p>
+              <p className="title is-1 is-size-1-tablet is-size-3-mobile">
+                FULL STACK DEVELOPING & MULTI-PHYSICS SIMULATION
+              </p>
+              <p className="subtitle is-3 is-size-6 is-weight-normal has-text-grey-dark">
+                If you are interested, please check my Resume through the button
+                below.
+              </p>
               <div className="btn-resume">
-                <a className="button is-large is-grey is-outlined is-radiusless" href="https://wangxxweb.herokuapp.com/"><p className="btn-text">Resume</p></a>
+                <a
+                  className="button is-large is-grey is-outlined is-radiusless"
+                  href="https://drive.google.com/file/d/1XCY-OvDPLs_BhHfaVV92Kh_77iBFe_aU/view"
+                >
+                  <p className="btn-text">Resume</p>
+                </a>
               </div>
             </div>
             <div className="skill-content column is-4-desktop is-offset-1-desktop is-11-touch is-offset-1-touch">
               <h2 className="content-title has-text-grey">WEB DEVELOP</h2>
               <div className="content-text">
-                <p>I grew my interest and skills in web developing by learning by myself through resources on the Internet.</p>
+                <p>
+                  I grew my interest and skills in web developing by numerous
+                  resources on the Internet. Here are the skills I mainly use:
+                </p>
                 <dl>
-                  <dt><strong>Front end:</strong></dt>
-                  <dd>HTML, CSS, Javascript, React, Redux, Jquery, SASS, PostCSS, Bulma, Semantic UI, Bootstrap.</dd>
-                  <dt><strong>Back end:</strong></dt>
+                  <dt>
+                    <strong>Front end:</strong>
+                  </dt>
+                  <dd>
+                    HTML, CSS, Javascript, React, Redux, GraphQL, Vue, SASS,
+                    PostCSS.
+                  </dd>
+                  <dt>
+                    <strong>Back end:</strong>
+                  </dt>
                   <dd>Node.js, Express, MongoDB.</dd>
-                  <dt><strong>Tools:</strong></dt>
-                  <dd>Git, Webpack, Gulp, Github, Heroku.</dd>
+                  <dt>
+                    <strong>Tools:</strong>
+                  </dt>
+                  <dd>Git, Webpack, Heroku, Jest, Mocha and TypeScript.</dd>
                 </dl>
               </div>
               <h2 className="content-title has-text-grey">ACADEMIC</h2>
               <div className="content-text">
-                <p>Graduated from University of Victoria, I gained knowledge and the experiences in research from several projects.</p>
+                <p>
+                  Graduated from University of Victoria, I gained knowledge and
+                  the experiences in research from several projects.
+                </p>
                 <dl>
-                  <dt><strong>Software:</strong> </dt>
-                  <dd>COMSOL, SolidWorks, MATLAB, Office, Latex.</dd>
-                  <dt><strong>Skills:</strong></dt>
+                  <dt>
+                    <strong>Software:</strong>{" "}
+                  </dt>
+                  <dd>COMSOL, SolidWorks, MATLAB, MS Office, Latex.</dd>
+                  <dt>
+                    <strong>Skills:</strong>
+                  </dt>
                   <dd>Plans, Reserach, Simulation, Debug, Analyze, Reports.</dd>
                 </dl>
               </div>
               <h2 className="content-title has-text-grey">ONGOING</h2>
               <div className="content-text">
-                <p>While I'm keep on studying the techs I'm familiar with, I'm picking up new stuffs.</p>
+                <p>
+                  While I'm keep on studying the techs I'm familiar with, I'm
+                  going to pick up new stuffs.
+                </p>
                 <dl>
-                  <dt><strong>UI/UX Design:</strong></dt>
-                  <dd>Sketch, Lunacy, Adobe PhotoShop.</dd>
-                  <dt><strong>Others:</strong></dt>
-                  <dd>GraphQL,Vue, Python, Mithril, AWS, Hapi.js.</dd>
+                  <dt>
+                    <strong>Skills:</strong>
+                  </dt>
+                  <dd>Laravel, SQL, PostgreSQL, Python and D3.</dd>
                 </dl>
               </div>
             </div>
